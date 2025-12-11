560f762589b59f18a4453283330ec072c1dcfa65
FAR Labs <112399916+farlabs-dev@users.noreply.github.com>
2025-12-11 18:48:40 +0400

    minor fixes and comments added
    Comments added for readability

--- FarcanaToken.sol
+++ FarcanaToken.sol
@@ -1,22 +1,39 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.20;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable2Step.sol";
 
 contract FarcanaToken is ERC20, Ownable2Step {
-    event Burn(address indexed burner, uint256 value);
 
-	
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) Ownable(msg.sender) {
-        uint256 _decimals = 18;
-        uint256 _initialSupply = 5000000000 * 10 ** _decimals;
-        _mint(msg.sender, _initialSupply);
+    uint256 private constant DECIMALS = 18;
+    uint256 private constant INITIAL_SUPPLY = 5000000000;
+
+    /**
+     * @dev Constructor that gives msg.sender all of existing tokens.
+     * @param _name The name of the token
+     * @param _symbol The symbol of the token
+     */
+    constructor(
+        string memory _name, 
+        string memory _symbol
+    ) ERC20(_name, _symbol) Ownable(msg.sender) {
+        _mint(msg.sender, INITIAL_SUPPLY * 10 ** DECIMALS);
     }
 
-  function burn(uint256 _value) external onlyOwner {
-        _burn(msg.sender, _value);
-        emit Burn(msg.sender, _value);
-  }
- 
+    /**
+     * @dev Burns a specific amount of tokens.
+     * @param _value The amount of token to be burned.
+     */
+    function burn(uint256 _value) public {
+        require(_value > 0, "Amount must be greater than 0");
+        _burn(msg.sender, _value);        
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     */
+    function decimals() public pure override returns (uint8) {
+        return uint8(DECIMALS);
+    }
 }
